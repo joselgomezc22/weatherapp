ee6bbba3fa69439bad4995644c9250857b2e6f71
Jose Leomar Gomez <61978606+joselgomezc22@users.noreply.github.com>
2021-09-12 19:56:03 -0500

    Style Detailing & Forecast api petition repair

--- assets/index.js
+++ assets/index.js
@@ -15,7 +15,6 @@ function appendCurrentWheaterHtml(cardId,json){
     let city = json.name;
     let temp =  parseInt(json.main.temp);
     let data = json.weather[0];
-    console.log(data);
     let main = data.main;
     let description = data.description;
     let icon = data.icon;
@@ -37,19 +36,19 @@ function appendCurrentWheaterHtml(cardId,json){
 //////////////////////////////////////////////
 // Html '3 DAYS FORECAST Weather' data ById //
 /////////////////////////////////////////////
-function appendForecastWheaterHtml(cardId,json){
+function appendForecastWheaterHtml(cardId,json,cityname){
     let html = ``;
     let forecast = ``;
     let parentCard = document.getElementById(cardId);
-    let city = json.city.name;
+    let city = cityname;
     // id children generatie
     let childid = Math.random();
 
     html = `
         <div class="text-header">
-            <h1>3 DAYS FORECAST  FOR ${city} </h1>
+            <h1>3 Days Forecast for ${city}</h1>
         </div>
-        <div id="${childid}" class="grid-section">
+        <div id="${childid}" class="grid-section grid-responsive-m">
 
         </div>  
     
@@ -58,25 +57,35 @@ function appendForecastWheaterHtml(cardId,json){
     parentCard.innerHTML = html ;
 
     let cardson = document.getElementById(cardId);
-    // List iteration
-    json.list.forEach(element => {
-        
-        let temp =  parseInt(element.main.temp);
+    // List iteration (first 3 elements)
+   
+    for (var i = 0; i < 3; i++) {
+        let element = json[i]
+        let temp =  parseInt(element.temp.day);
         let data = element.weather[0];
-        let date = element.dt_txt;
+        const today = new Date()
+        const date = new Date(today)
+        date.setDate(date.getDate() + i +1);
+
+        var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
          
+        var dayName = days[date.getDay()];
+         
+
+                
         let main = data.main;
         let description = data.description;
         let icon = data.icon;
         // Adding the Forecast day Card
          forecast += `
          <div class="padding-small">
-         <div class="card-transparent ">
+         <div class="card-transparent grid-section">
                 <div class="padding-small">
-                    <h3>${date} weather in <span class="">${city}</span> </h3>
-                    <h3><span class="main">${main}</span></h3>
-                    <h1><span class="temperature">${temp}</span>°</h1>
-                    <h3><span class="description">${description}</span> </h3>
+                <p class="text-small">${dayName}</p>
+                <hr>
+                <h3><span class="main">${main}</span></h3>
+                <h1><span class="temperature">${temp}</span>°</h1>
+                <h3><span class="description">${description}</span> </h3>
                 </div>
                 <div class="padding-small">
                     <div class="icon">
@@ -87,7 +96,7 @@ function appendForecastWheaterHtml(cardId,json){
         </div>
         </div>`;
           
-    });
+    };
     // Wheather Cards Html 
     let childdiv = document.getElementById(childid);
     childdiv.innerHTML = forecast;
@@ -98,13 +107,27 @@ function appendForecastWheaterHtml(cardId,json){
 const ApiUrl = 'https://api.openweathermap.org/data/2.5/';
 const apiToken = '2c689ad51f75b6467af0c2069385327d';
 
+// Get Current wheater, cords and daily forecast by a cityname 
+function GetGlobalWeather(cityname,apiuri,apitkn){
+    let current = httpGet(apiuri+'weather?appid='+apitkn+'&units=metric&q='+cityname);
+    let city    =  current.name;
+    let coords  =  current.coord;
+    let daily   = httpGet(apiuri+'onecall?appid='+apitkn+'&lat='+coords.lat+'&lon='+coords.lon+'&exclude=hourly,current,minutely&units=metric');
+    return {'current':current,'daily':daily.daily,'city':city};
+}
+console.log(GetGlobalWeather('toronto',ApiUrl,apiToken));
+
 ////////////////////////////////////
 // Global Weather Search Function //
 ////////////////////////////////////
 function globalSearch(city,apiuri,apitkn){
-    appendCurrentWheaterHtml('main-weather-card',httpGet(apiuri+'weather?appid='+apitkn+'&units=metric&q='+city));
-    appendForecastWheaterHtml('forecast-main',httpGet(apiuri+'forecast?appid='+apitkn+'&cnt=3&units=metric&q='+city));
+    // Get Global Data
+    let GData = GetGlobalWeather(city,ApiUrl,apiToken);
+    appendCurrentWheaterHtml('main-weather-card',GData.current);
+    appendForecastWheaterHtml('forecast-main',GData.daily,GData.city);
 }
+
+
 // First Seach (1st Bogota)
 globalSearch('bogota',ApiUrl,apiToken);
 // Extra Search for Current paris weather
